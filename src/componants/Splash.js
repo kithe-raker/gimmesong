1ce47a291cd758f3008a9bb125166f127d6ae8a9
BoSoJI <57142176+SB051@users.noreply.github.com>
2022-10-24 20:37:03 +0700

    add: some splash ui

---
+++ src/componants/Splash.js
@@ -0,0 +1,14 @@
+import disc from "../disc.svg";
+import React from 'react'
+
+function Splash() {
+    return (
+        <div className='min'>
+                <div className="w-full h-full place-content-center">
+                    <img className="w-32" src={disc} alt="disc" />
+                </div>
+        </div>
+    )
+}
+
+export default Splash
