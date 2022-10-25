d44394092e509937a267b854ecc51179ad4f63b9
taritinth <tarit.khanbo@hotmail.com>
2022-10-25 19:30:18 +0700

    edit: sent component

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PasteLink from "./components/PasteLink.js";
-import OnSendComplete from "./components/OnComplete.js";
+import Sent from "./components/Sent.js";
 
 function Search() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen max-w-md mx-auto">
-      <PasteLink />
+      <Sent />
       <div className="flex fixed bottom-10 w-full flex-col items-center">
         {/* <button className=" h-12 w-[250px] gimmesong-primary-font bg-black rounded-full text-white">
           NEXT
---
+++ src/pages/Search/components/Sent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import bg from "@assets/img/sendComplete_bg.png";
+import Disc from "@assets/img/disc.svg";
+
+function Sent() {
+  return (
+    <>
+      <div className="relative h-48 w-72 flex flex-col items-center justify-center overflow-hidden rounded-3xl">
+        <img
+          className="absolute h-full w-full inset-0 object-cover"
+          src={bg}
+          alt="Congrate"
+        />
+        <div className="absolute flex w-64 flex-col items-center">
+          <h1 className="gimmesong-primary-font text-white text-3xl mb-[5px]">
+            Congratulation
+          </h1>
+          <span className="text-white text-xs mb-[14px]">
+            Your song already given!
+          </span>
+          <img className="h-[71px]" src={Disc} alt="disc" />
+        </div>
+      </div>
+      <span className="gimmesong-primary-font mt-4 text-sm">
+        Songs have been given 123,456 times.
+      </span>
+    </>
+  );
+}
+
+export default Sent;
