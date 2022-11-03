2a09f9061f22e2e71ddbd09029bc4e65eb9f0ab8
taritinth <tarit.khanbo@hotmail.com>
2022-11-03 14:03:16 +0700

    edit: header & my acc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,13 @@ function Header() {
     <header className="fixed top-0 right-0 left-0 mx-auto flex h-[60px] w-full max-w-md items-center justify-end px-2">
       <button className="flex h-[42px] w-[42px] shrink-0 items-center justify-center rounded-full bg-black shadow-sm hover:bg-gray-700">
         <svg
+          className="text-white"
           xmlns="http://www.w3.org/2000/svg"
           width="20"
           height="20"
           viewBox="0 0 24 24"
           fill="none"
-          stroke="#FFFFFF"
+          stroke="currentColor"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
