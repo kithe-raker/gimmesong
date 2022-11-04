22d034811a77ff75d3bf60e75f35b2b9a67b2e7d
taritinth <tarit.khanbo@hotmail.com>
2022-11-04 20:00:14 +0700

    edit: navbar icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,8 +24,10 @@ function Header() {
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            <rect x="3" y="3" width="7" height="7"></rect>
+            <rect x="14" y="3" width="7" height="7"></rect>
+            <rect x="14" y="14" width="7" height="7"></rect>
+            <rect x="3" y="14" width="7" height="7"></rect>
           </svg>
         </button>
       )}
