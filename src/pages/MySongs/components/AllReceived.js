65930624e61aa5e7bfda412b0eab13d9e0a3c695
taritinth <tarit.khanbo@hotmail.com>
2022-11-05 01:42:36 +0700

    style: change share icon

--- src/pages/MySongs/components/AllReceived.js
+++ src/pages/MySongs/components/AllReceived.js
@@ -482,9 +482,11 @@ function AllReceived({ layout, onLayoutChange }) {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   >
-                    <g fill="none" fillRule="evenodd">
-                      <path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8" />
-                    </g>
+                    <circle cx="18" cy="5" r="3"></circle>
+                    <circle cx="6" cy="12" r="3"></circle>
+                    <circle cx="18" cy="19" r="3"></circle>
+                    <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
+                    <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
                   </svg>
                 </button>
               </div>
