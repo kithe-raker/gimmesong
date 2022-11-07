72acee23f696ca4a3cf203583b52d9f35dca378f
taritinth <tarit.khanbo@hotmail.com>
2022-11-08 00:00:01 +0700

    style: svg fill color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,7 +43,7 @@ function Header() {
               height="20"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#FFFFFF"
+              fill="currentColor"
             >
               <path
                 stroke="currentColor"
--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -82,7 +82,7 @@ function Menu() {
             height="21"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
-            fill="#FFFFFF"
+            fill="currentColor"
           >
             <path
               stroke="currentColor"
