c0e75f7dea620680b8faa5f30cc4dc0b8e1fec93
taritinth <tarit.khanbo@hotmail.com>
2022-11-09 00:12:42 +0700

    edit: loading state when exporting

--- src/pages/MySongs/components/AllReceived.js
+++ src/pages/MySongs/components/AllReceived.js
@@ -48,6 +48,7 @@ function AllReceived({ layout, onLayoutChange }) {
 
   const [exportMode, setExportMode] = useState("widget");
   const exportRef = useRef();
+  const [exporting, setExporting] = useState(false);
 
   const {
     audioRef,
@@ -83,33 +84,50 @@ function AllReceived({ layout, onLayoutChange }) {
 
   const exportImage = (inboxId) => {
     if (!exportRef.current || !inboxId) return;
+    if (exporting) return;
     htmlToPng(exportRef.current, inboxId);
   };
 
   const htmlToPng = useCallback(async (element, inboxId) => {
     if (!element) return;
 
-    const width = element.clientWidth;
-    const height = element.clientHeight;
+    try {
+      setExporting(true);
 
-    const canvas = await html2canvas(element, {
-      height,
-      width,
-      backgroundColor: null,
-      useCORS: true,
-    });
+      const width = element.clientWidth;
+      const height = element.clientHeight;
 
-    // document.body.appendChild(canvas);
+      const canvas = await html2canvas(element, {
+        height,
+        width,
+        backgroundColor: null,
+        useCORS: true,
+      });
 
-    let a = document.createElement("a");
-    a.download = `inbox-${exportMode}-${inboxId}.png`;
+      // document.body.appendChild(canvas);
 
-    canvas.toBlob((blob) => {
-      const url = URL.createObjectURL(blob);
-      a.href = url;
-      a.click();
-      onCloseModal();
-    });
+      let a = document.createElement("a");
+      a.download = `inbox-${exportMode}-${inboxId}.png`;
+
+      canvas.toBlob((blob) => {
+        const url = URL.createObjectURL(blob);
+        a.href = url;
+        a.click();
+
+        onCloseModal();
+      });
+    } catch (err) {
+      toast("Export failed", {
+        style: {
+          borderRadius: "25px",
+          background: "#FF6464",
+          color: "#fff",
+        },
+      });
+      console.error(err);
+    } finally {
+      setExporting(false);
+    }
   }, []);
 
   const handleSelect = (index) => {
@@ -830,18 +848,41 @@ function AllReceived({ layout, onLayoutChange }) {
                         h={42}
                         ml={3}
                       >
-                        <svg
-                          className="mr-2 h-4 w-4 text-white"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5" />
-                        </svg>
+                        {exporting ? (
+                          <svg
+                            className="mr-2 h-4 w-4 animate-spin text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle
+                              className="opacity-25"
+                              cx="12"
+                              cy="12"
+                              r="10"
+                              stroke="currentColor"
+                              strokeWidth="4"
+                            ></circle>
+                            <path
+                              className="opacity-75"
+                              fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                          </svg>
+                        ) : (
+                          <svg
+                            className="mr-2 h-4 w-4 text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5" />
+                          </svg>
+                        )}
                         Export
                       </Button>
                       {/* {activeStep !== 4 ? (
