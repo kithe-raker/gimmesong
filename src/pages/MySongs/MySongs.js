928c8a471e303a7a461556e14c6f5b013c1efbde
taritinth <tarit.khanbo@hotmail.com>
2022-11-08 18:23:30 +0700

    style: justify evenly tab & switch

--- src/pages/MySongs/MySongs.js
+++ src/pages/MySongs/MySongs.js
@@ -29,7 +29,7 @@ function MySongs() {
 
   return (
     <div className="relative mx-auto flex max-h-screen max-w-md flex-col items-center overflow-hidden py-6 pt-[60px]">
-      <div className="mt-[72px] flex w-full items-center justify-between px-4">
+      <div className="mt-[72px] flex w-full items-center justify-evenly px-4">
         <div></div>
         <div className="flex items-center">
           <h1
