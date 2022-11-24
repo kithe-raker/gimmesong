0ba0debf889101ea73ce1580a1ca11802f55bb9a
kithe-raker <kithe.raker@gmail.com>
2022-11-24 23:31:35 +0700

    edit: change adsterra banner to adsence

--- src/components/Adsense/SearchSongBanner.js
+++ src/components/Adsense/SearchSongBanner.js
@@ -10,7 +10,7 @@ export default class SearchSongBanner extends React.Component {
       <ins
         className="adsbygoogle"
         style={{ display: "block" }}
-        data-ad-format="auto"
+        // data-ad-format="auto"
         data-full-width-responsive="true"
         data-ad-layout-key="-f9+5u+4s-dn+7k"
         data-ad-client="ca-pub-9146142853898618"
