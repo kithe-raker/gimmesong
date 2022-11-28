4e632ef3382e0feb2feff387b52506c75c237c99
taritinth <tarit.khanbo@hotmail.com>
2022-11-28 15:41:21 +0700

    test: just test open browser

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -57,6 +57,10 @@ function Home() {
       : signInWithGoogle();
   };
 
+  const openDefaultBrowser = () => {
+    window.open("https://gimmesong.link", "_system", "location=yes");
+  };
+
   return (
     <div className="mx-auto flex min-h-screen max-w-md flex-col items-center justify-center py-[60px] pt-[80px]">
       <span className="gimmesong-primary-font">
@@ -110,6 +114,12 @@ function Home() {
           <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
         </svg>
         <span>Continue with Google</span>
+      </button>
+      <button
+        onClick={openDefaultBrowser}
+        className="mt-2 flex h-12 w-[260px] items-center justify-center rounded-full bg-black font-bold text-white transition duration-150 ease-in-out hover:bg-gray-600"
+      >
+        <span>Open Browser</span>
       </button>
 
       <div className="mt-4 flex">
