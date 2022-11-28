37e2ce9cd5d8fc96d6054a1cbd8aec17ebc65a46
BoSoJI <57142176+SB051@users.noreply.github.com>
2022-11-28 21:35:19 +0700

    fix: svg invalid DOM property

--- src/features/Tutorials/components/AppShortcut.js
+++ src/features/Tutorials/components/AppShortcut.js
@@ -109,9 +109,9 @@ function AppShortcut() {
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="#000000"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         >
                           <line x1="12" y1="5" x2="12" y2="19"></line>
                           <line x1="5" y1="12" x2="19" y2="12"></line>
@@ -308,9 +308,9 @@ function AppShortcut() {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="#000000"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       >
                         <circle cx="12" cy="12" r="1"></circle>
                         <circle cx="12" cy="5" r="1"></circle>
