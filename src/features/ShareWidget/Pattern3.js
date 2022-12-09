7d503d7497bee056ea87207b0c51014aafa36c9f
BoSoJI <57142176+SB051@users.noreply.github.com>
2022-12-08 22:38:31 -0800

    Add new share widget (#55)
    * add: loading spinner
    
    * add: share navigator check
    
    * Fix: navigator.share()
    
    * fix: navigator.share()
    
    * add: exportFile to useImageExporter()
    
    * fix: shareImage -> shareImage()
    
    * Edit: share title
    
    * fix: shareImage function is duplicate
    
    * edit: add padding to share button
    
    * Add: show link in image state
    
    for image sharing in request song
    
    * Edit:icon at twitter share button
    
    * edit: gimmesong logo vertical align
    
    * fix: link is not show in mysong
    
    * edit: default text
    
    * edit: default text link
    
    * Add: image sharing border twitter
    
    * Add: new share selection (not finish)
    
    * Add: share function
    
    add loading spinner in pattern1

---
+++ src/features/ShareWidget/Pattern3.js
@@ -0,0 +1,109 @@
+import { useEffect } from "react";
+
+import disc from "@assets/img/disc.webp";
+import logo from "@assets/img/gimmesong_logo.png";
+
+import { ThreeDots } from "react-loader-spinner";
+import { useImageExporter } from "@hooks/useImageExporter";
+import useSession from "@hooks/useSession";
+
+const Pattern3 = ({ content, isMysong, onSharing }) => {
+  const { exportedURL, exportedFile, exportRefCallback } = useImageExporter();
+  const { user } = useSession();
+
+  useEffect(() => {
+    if (!exportedFile) return;
+    onSharing(exportedFile);
+  }, [exportedFile, onSharing]);
+
+  return (
+    <>
+      <div className="h-0 w-0 overflow-hidden">
+        <div
+          className="flex w-[960px] flex-col justify-between bg-white px-[86px] pt-[42px] pb-[86px]"
+          ref={exportRefCallback}
+        >
+          <div>
+            <div className="flex items-center">
+              <div className="relative mt-[40px] flex h-[180px] w-[180px] shrink-0 items-center justify-center">
+                <img
+                  className="absolute inset-0 h-full w-full select-none object-contain"
+                  src={disc}
+                  alt="disc"
+                />
+                <img
+                  className="absolute h-[52px] w-[52px] shrink-0 rounded-full object-contain "
+                  src={content.song.thumbnails[0].url}
+                  alt="thumbnail"
+                  referrerPolicy="no-referrer"
+                  crossOrigin="anonymous"
+                />
+              </div>
+              <div className="ml-[32px] flex min-w-0 flex-col ">
+                <span className={`truncate text-[44px] font-bold leading-[2] `}>
+                  {content.song.title}
+                </span>
+                <span
+                  className={` -mt-[28px] truncate text-[42px] font-light leading-[2] text-gray-400`}
+                >
+                  {content.song.artist}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="mt-[48px] mb-[64px] w-full px-[8px]">
+            <p
+              style={{
+                wordBreak: "break-word",
+                whiteSpace: "pre-line",
+                // "-webkit-text-stroke": "0.5px white",
+              }}
+              className=" pr-[120px] text-[54px]  font-bold italic text-black"
+            >
+              {content.message}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex items-center">
+              <img
+                className="mr-[10px] mt-[29px] h-[36px] w-[36px]"
+                src={logo}
+                alt="disc"
+              />
+              <span className="gimmesong-primary-font text-[36px] tracking-wider">
+                GIMMESONG
+              </span>
+            </div>
+            {isMysong && (
+              <span className="-mt-[20px]  text-[36px] text-gray-300">
+                gimmesong.link/@{user.username}
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-3 flex flex-col items-center rounded-3xl border">
+        {exportedURL ? (
+          <img
+            className="w-full rounded-3xl"
+            src={exportedURL}
+            alt={`inbox-widget`}
+          />
+        ) : (
+          <ThreeDots
+            height="60"
+            width="60"
+            radius="9"
+            color="#8583D6"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+export default Pattern3;
--- src/hooks/useShareDialog.js
+++ src/hooks/useShareDialog.js
@@ -8,26 +8,29 @@ import {
   AlertDialogContent,
   AlertDialogOverlay,
   AlertDialogCloseButton,
-  AlertDialogFooter,
+  Button,
 } from "@chakra-ui/react";
 
-import Instagram from "@features/ShareWidget/Instagram";
-import Twitter from "@features/ShareWidget/Twitter";
+import useSession from "@hooks/useSession";
+
+import Pattern2 from "@features/ShareWidget/Pattern3";
+import Pattern1 from "@features/ShareWidget/Pattern1";
 
 export const useShareDialog = () => {
-  const [share, setShare] = useState("menu");
+  const { user } = useSession();
+
+  const [pattern, setPattern] = useState(1);
+  const [file, setFile] = useState(null);
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = useRef();
 
-  const closeShareDialog = () => {
-    onClose();
-    setShare("menu");
-  };
-
   const openShareDialog = () => {
     onOpen();
-    setShare("menu");
+  };
+
+  const handleSharing = (file) => {
+    setFile(file);
   };
 
   const ShareDialog = useCallback(
@@ -37,7 +40,7 @@ export const useShareDialog = () => {
           <AlertDialog
             motionPreset="slideInBottom"
             leastDestructiveRef={cancelRef}
-            onClose={closeShareDialog}
+            onClose={onClose}
             isOpen={isOpen}
             isCentered
             size="md"
@@ -52,49 +55,110 @@ export const useShareDialog = () => {
                 justifyContent={`center`}
                 alignItems={`center`}
               >
-                {/* select */}
-                {share === "menu" && (
-                  <div className="mb-6 flex w-full flex-col items-center">
+                <div className="flex w-full flex-col items-center justify-center">
+                  {pattern === 1 && (
+                    <Pattern1 content={content} onSharing={handleSharing} />
+                  )}
+                  {pattern === 2 && (
+                    <Pattern2
+                      content={content}
+                      isMysong={isMysong}
+                      onSharing={handleSharing}
+                    />
+                  )}
+                  {/* {pattern === "3" && (
+                    <Pattern1 content={(content, isMysong)} />
+                  )} */}
+                  <div className="my-4 flex h-10 w-[137px] shrink-0 items-center  justify-around rounded-full border">
                     <button
-                      className="mb-2 flex w-full items-center justify-center rounded-full border py-3 text-xl"
-                      onClick={() => setShare("instagram")}
+                      onClick={() => setPattern(1)}
+                      className={`h-8 w-8 rounded-full ${
+                        pattern === 1
+                          ? "bg-black text-white"
+                          : "bg-white text-black"
+                      }`}
                     >
-                      <svg
-                        className="mr-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="#000"
-                      >
-                        <path d="M16.98 0a6.9 6.9 0 0 1 5.08 1.98A6.94 6.94 0 0 1 24 7.02v9.96c0 2.08-.68 3.87-1.98 5.13A7.14 7.14 0 0 1 16.94 24H7.06a7.06 7.06 0 0 1-5.03-1.89A6.96 6.96 0 0 1 0 16.94V7.02C0 2.8 2.8 0 7.02 0h9.96zm.05 2.23H7.06c-1.45 0-2.7.43-3.53 1.25a4.82 4.82 0 0 0-1.3 3.54v9.92c0 1.5.43 2.7 1.3 3.58a5 5 0 0 0 3.53 1.25h9.88a5 5 0 0 0 3.53-1.25 4.73 4.73 0 0 0 1.4-3.54V7.02a5 5 0 0 0-1.3-3.49 4.82 4.82 0 0 0-3.54-1.3zM12 5.76c3.39 0 6.2 2.8 6.2 6.2a6.2 6.2 0 0 1-12.4 0 6.2 6.2 0 0 1 6.2-6.2zm0 2.22a3.99 3.99 0 0 0-3.97 3.97A3.99 3.99 0 0 0 12 15.92a3.99 3.99 0 0 0 3.97-3.97A3.99 3.99 0 0 0 12 7.98zm6.44-3.77a1.4 1.4 0 1 1 0 2.8 1.4 1.4 0 0 1 0-2.8z" />
-                      </svg>
-                      Instagram
+                      1
                     </button>
                     <button
-                      className="flex w-full items-center justify-center rounded-full border py-3 text-xl"
-                      onClick={() => setShare("twitter")}
+                      onClick={() => setPattern(2)}
+                      className={`h-8 w-8 rounded-full ${
+                        pattern === 2
+                          ? "bg-black text-white"
+                          : "bg-white text-black"
+                      }`}
                     >
-                      <svg
-                        className="mr-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="#000"
-                      >
-                        <path d="M20 0a4 4 0 0 1 4 4v16a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h16zm-4.42 5.8a3.28 3.28 0 0 0-3.2 4.03A9.32 9.32 0 0 1 5.61 6.4a3.26 3.26 0 0 0 1.02 4.38 3.27 3.27 0 0 1-1.49-.4v.03a3.28 3.28 0 0 0 2.64 3.22 3.3 3.3 0 0 1-1.49.06 3.29 3.29 0 0 0 3.07 2.28 6.59 6.59 0 0 1-4.86 1.36 9.29 9.29 0 0 0 5.03 1.47c6.04 0 9.34-5 9.34-9.34v-.42a6.65 6.65 0 0 0 1.63-1.7c-.59.26-1.22.44-1.89.52.68-.41 1.2-1.05 1.45-1.82-.64.38-1.34.65-2.09.8a3.28 3.28 0 0 0-2.4-1.04z" />
-                      </svg>
-                      Twitter
+                      2
                     </button>
+                    {/* <button
+                      onClick={setPattern("3")}
+                      className={`h-8 w-8 rounded-full ${
+                        pattern === "3"
+                          ? "bg-black text-white"
+                          : "bg-white text-black"
+                      }`}
+                    >
+                      3
+                    </button> */}
                   </div>
-                )}
-                {/* Instagram */}
-                {share === "instagram" && <Instagram content={content} />}
-                {/* Twitter */}
-                {share === "twitter" && (
-                  <Twitter content={content} isMysong={isMysong} />
-                )}
+                  <div className="flex w-full justify-between">
+                    <Button
+                      onClick={() => {
+                        const element = document.createElement("a");
+                        element.href = URL.createObjectURL(file);
+                        element.download = "gimmesong-" + Date.now() + ".png";
+
+                        document.body.appendChild(element); // Required for this to work in FireFox
+                        element.click();
+                      }}
+                      w="full"
+                      borderRadius="25"
+                      bgColor="black"
+                      color="white"
+                      h={42}
+                      _hover={{ bg: "#000000" }}
+                      _active={{
+                        bg: "#000000",
+                      }}
+                    >
+                      Download
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        if (navigator.canShare({ files: [file] })) {
+                          try {
+                            navigator.share({
+                              files: [file],
+                              title: this.ShareDialog.isMysong
+                                ? `@${user.username}`
+                                : "",
+                              text: "#gimmsong #gimmesonglink",
+                            });
+                          } catch (err) {
+                            console.error(err);
+                          }
+                        } else {
+                          console.log("cannot");
+                        }
+                      }}
+                      w="full"
+                      borderRadius="25"
+                      bgColor="black"
+                      color="white"
+                      ml={3}
+                      h={42}
+                      _hover={{ bg: "#000000" }}
+                      _active={{
+                        bg: "#000000",
+                      }}
+                    >
+                      Share
+                    </Button>
+                  </div>
+                  <span className="pt-2 pb-1 text-center">
+                    Or long press at the image to save.
+                  </span>
+                </div>
               </AlertDialogBody>
               {/* <AlertDialogFooter></AlertDialogFooter> */}
             </AlertDialogContent>
@@ -102,7 +166,7 @@ export const useShareDialog = () => {
         </>
       );
     },
-    [isOpen, share]
+    [isOpen, onClose, pattern]
   );
 
   return { openShareDialog, ShareDialog };
