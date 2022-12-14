d51450587bfa779f021fc43b728f0bddd6a4eefa
athensclub <43963760+athensclub@users.noreply.github.com>
2022-12-15 01:31:39 +0700

    edit: remove old, commented code in view playlist page

--- src/features/RequestSongs/ViewPlaylist/ViewPlaylist.js
+++ src/features/RequestSongs/ViewPlaylist/ViewPlaylist.js
@@ -15,7 +15,6 @@ function ViewPlaylist() {
   const navigate = useNavigate();
   const { id: shareLinkId } = useParams();
 
-
   // Call VignetteBanner ads
   Ads.VignetteBanner();
 
@@ -35,16 +34,10 @@ function ViewPlaylist() {
         pageLayout === "single" ? "h-full" : "min-h-full"
       } max-w-md flex-col items-center py-6 pt-64`}
     >
-      {/* <div className="gimmesong-bg fixed top-0 z-50 flex h-16 w-full max-w-md items-center justify-between px-2.5">
-        
-      </div> */}
       {isLoadingInfo ? (
         <Loading fullScreen />
       ) : playlistInfo ? (
         <div className="flex w-full max-w-md flex-col items-center">
-          {/* <div className="gimmesong-bg sticky top-[60px] z-[49] mt-4 flex w-full flex-col items-center p-3">
-            
-          </div> */}
           <ReceivedSongs layout={pageLayout} onLayoutChange={setPageLayout} />
         </div>
       ) : (
