dbd29895c30d5a144605c066deb5ceb25148204d
athensclub <43963760+athensclub@users.noreply.github.com>
2022-12-16 02:30:53 +0700

    edit: new style for grid/full view button

--- src/components/Header/components/OnViewPlaylistPage.js
+++ src/components/Header/components/OnViewPlaylistPage.js
@@ -13,6 +13,7 @@ import AddSongModal from "@features/RequestSongs/ViewPlaylist/AddSongModal";
 import heartEmoji from "@assets/img/heart_emoji.png";
 import headphoneEmoji from "@assets/img/headphone_emoji.png";
 import disc from "@assets/img/disc.png";
+import SelectPageLayout from "./SelectPageLayout";
 
 function OnViewPlaylistPage() {
   const navigate = useNavigate();
@@ -135,107 +136,10 @@ function OnViewPlaylistPage() {
               </span>
             </button>
 
-            <div className="inline-flex rounded-full" role="group">
-              <button
-                type="button"
-                onClick={() => setPageLayout("single")}
-                className={`rounded-l-xl ${
-                  pageLayout === "single"
-                    ? "bg-black text-white"
-                    : "bg-white text-black"
-                } h-10 min-w-[56px] py-2 px-4 text-sm font-medium`}
-              >
-                <div className="flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="23"
-                    height="13"
-                    viewBox="0 0 23 13"
-                    fill="none"
-                  >
-                    <rect
-                      x="5"
-                      width="13"
-                      height="13"
-                      rx="6.5"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M0 4H0.5C1.88071 4 3 5.11929 3 6.5C3 7.88071 1.88071 9 0.5 9H0V4Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M23 9H22.5C21.1193 9 20 7.88071 20 6.5C20 5.11929 21.1193 4 22.5 4H23V9Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span className="gimmesong-secondary-font ml-2 text-sm">
-                    Full
-                  </span>
-                </div>
-              </button>
-              <button
-                type="button"
-                onClick={() => setPageLayout("multiple")}
-                className={`rounded-r-xl ${
-                  pageLayout === "multiple"
-                    ? "bg-black text-white"
-                    : "bg-white text-black"
-                } h-10 min-w-[56px] py-2 px-4 text-sm font-medium`}
-              >
-                <div className="flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="9"
-                    height="15"
-                    viewBox="0 0 9 15"
-                    fill="none"
-                  >
-                    <rect width="3" height="3" rx="1.5" fill="currentColor" />
-                    <rect
-                      y="6"
-                      width="3"
-                      height="3"
-                      rx="1.5"
-                      fill="currentColor"
-                    />
-                    <rect
-                      y="12"
-                      width="3"
-                      height="3"
-                      rx="1.5"
-                      fill="currentColor"
-                    />
-                    <rect
-                      x="6"
-                      width="3"
-                      height="3"
-                      rx="1.5"
-                      fill="currentColor"
-                    />
-                    <rect
-                      x="6"
-                      y="6"
-                      width="3"
-                      height="3"
-                      rx="1.5"
-                      fill="currentColor"
-                    />
-                    <rect
-                      x="6"
-                      y="12"
-                      width="3"
-                      height="3"
-                      rx="1.5"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span className="gimmesong-secondary-font ml-2 text-sm">
-                    Grid
-                  </span>
-                </div>
-              </button>
-            </div>
+            <SelectPageLayout
+              setPageLayout={setPageLayout}
+              pageLayout={pageLayout}
+            />
           </div>
         </div>
       )}
---
+++ src/components/Header/components/SelectPageLayout.js
@@ -0,0 +1,69 @@
+import { useState, useContext, useEffect } from "react";
+
+import { PlaylistContext } from "contexts/PlaylistContext";
+import Dropdown from "@components/Dropdown";
+
+function SelectPageLayout({ setPageLayout, pageLayout }) {
+  const single = (
+    <div className="flex w-full items-center justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="23"
+        height="13"
+        viewBox="0 0 23 13"
+        fill="white"
+      >
+        <rect x="5" width="13" height="13" rx="6.5" fill="white" />
+        <path
+          d="M0 4H0.5C1.88071 4 3 5.11929 3 6.5C3 7.88071 1.88071 9 0.5 9H0V4Z"
+          fill="white"
+        />
+        <path
+          d="M23 9H22.5C21.1193 9 20 7.88071 20 6.5C20 5.11929 21.1193 4 22.5 4H23V9Z"
+          fill="white"
+        />
+      </svg>
+      <span className="gimmesong-secondary-font ml-3 text-sm text-white">
+        Full
+      </span>
+    </div>
+  );
+
+  const multiple = (
+    <div className="flex w-full items-center justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="9"
+        height="15"
+        viewBox="0 0 9 15"
+        fill="white"
+      >
+        <rect width="3" height="3" rx="1.5" fill="white" />
+        <rect y="6" width="3" height="3" rx="1.5" fill="white" />
+        <rect y="12" width="3" height="3" rx="1.5" fill="white" />
+        <rect x="6" width="3" height="3" rx="1.5" fill="white" />
+        <rect x="6" y="6" width="3" height="3" rx="1.5" fill="white" />
+        <rect x="6" y="12" width="3" height="3" rx="1.5" fill="white" />
+      </svg>
+      <span className="gimmesong-secondary-font ml-3 text-sm text-white">
+        Grid
+      </span>
+    </div>
+  );
+
+  const options = [
+    { value: "single", component: single },
+    { value: "multiple", component: multiple },
+  ];
+
+  return (
+    <Dropdown
+      options={options}
+      onOptionSelected={setPageLayout}
+      className="w-24 h-full"
+      contentClassName="rounded-lg bg-black py-1"
+    />
+  );
+}
+
+export default SelectPageLayout;
