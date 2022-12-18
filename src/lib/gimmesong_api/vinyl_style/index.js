6044db5f9a4c861cbc860248a5cba5ccbee86fa1
kithe-raker <kithe.raker@gmail.com>
2022-12-18 15:49:47 +0700

    feat: create get vinyl style details function #65

---
+++ src/lib/gimmesong_api/vinyl_style/index.js
@@ -0,0 +1,29 @@
+import logo from "@assets/img/gimmesong_logo.png";
+
+const __VinylStyle = {
+  background: {
+    id: { id: "id", image_url: logo },
+  },
+  center: {
+    id: { id: "id", image_url: logo },
+  },
+};
+
+const methods = {
+  /**
+   *
+   * @param {*} type right now we only have [background] and [center] vinyl component's type
+   * @param {*} id
+   * @returns
+   */
+  getStyleDetails: async function (type, id) {
+    if (!id) throw "no id provided";
+    if (!type) throw "no type provided";
+    if (type != "background" && type != "center")
+      throw "provided type not exists";
+
+    return __VinylStyle[type][id]
+      ? { exists: true, details: __VinylStyle[type][id] }
+      : { exists: false };
+  },
+};
