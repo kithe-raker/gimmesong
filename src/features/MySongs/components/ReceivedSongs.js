521f709f07811a28d5b2ff755c1c5b9a85cad7a1
athensclub <43963760+athensclub@users.noreply.github.com>
2022-12-24 23:02:48 +0700

    edit: remove unnecessary logging

--- src/features/MySongs/components/ReceivedSongs.js
+++ src/features/MySongs/components/ReceivedSongs.js
@@ -239,7 +239,6 @@ function ReceivedSongs({ tab, layout, onLayoutChange }) {
   };
 
   const handleTrackChange = async () => {
-    console.log("current", current, "setting", playerSetting);
     if (playerSetting.autoplay) {
       clearUpNextTimer();
       if (!items[current]?.played) await handleUpdateInbox(items[current]?.id);
